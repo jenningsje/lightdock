370f655ad52c022a4d6e0a72307f71e176045352
James <jenningsje@mac.com>
2024-03-20 11:28:45 -0500

    adjust lightdock for impurities

--- lightdock/structure/mass.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-# Read the Excel file into a DataFrame
-mass_df = pd.read_excel('mass.xlsx')
-
-# Convert DataFrame to dictionary
-mass_dict = mass_df.set_index('element')['mass'].to_dict()
--- run_node.py
+++ run_node.py
@@ -1,3 +1,12 @@
 import subprocess
 
-subprocess.run("fetch_pdbs.ts", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+print("fetching pdbs")
+subprocess.run("node fetch_pdbs.ts", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+print("setting up lightdock")
+subprocess.run("node lightdock_setup.ts", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+print("running lightdock")
+subprocess.run("node lightdock_run.ts", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+print("fetching conformation")
+subprocess.run("node ./bin/generate_confs.ts", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+print("cleaning directory")
+subprocess.run("rm *.pdb")
